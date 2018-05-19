d369997e93394a9316b85d235a83467074326f72
raibosome <raimi.bkarim@gmail.com>
2018-05-19 16:42:16 +0800

    Removed "Change Language" option. Removed onCreateOptionsMenu, onOptionsItemSelected, and menu layout

--- app/src/main/java/com/raimikarim/thekiasushopper/MainActivity.java
+++ app/src/main/java/com/raimikarim/thekiasushopper/MainActivity.java
@@ -184,29 +184,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-////        int id = item.getItemId();
-////
-////        //noinspection SimplifiableIfStatement
-////        if (id == R.id.action_language) {
-////            Toast.makeText(getApplicationContext(), "Feature currently unavailable", Toast.LENGTH_SHORT).show();
-////            return true;
-////        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -232,7 +209,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
             Intent intent = new Intent(Intent.ACTION_VIEW,
                     Uri.parse("market://details?id=com.raimikarim.thekiasushopper"));
-            startActivity(intent);
 
         } else if (id == R.id.grocery_list) {
             Toast.makeText(MainActivity.this, "Feature currently unavailable", Toast.LENGTH_SHORT).show();
